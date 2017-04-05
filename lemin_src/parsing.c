7e046fc935353e09d056f2c388d1f63b0d5b2ba1
sle-lieg <sle-lieg@sle-liegs-Mac.local>
2017-04-05 06:21:40 -0700

    lemin

--- lemin_src/parsing.c
+++ lemin_src/parsing.c
@@ -2,30 +2,63 @@
 
 void 	ft_parse(t_lemin *lem)
 {
+	ft_get_ants(lem);
+	ft_get_rooms(lem);
+	ft_get_links(lem);
+}
+
+void 	ft_get_ants(t_lemin *lem)
+{
+	//		ANTS
+	//nombre fourmis == une chaine de digit sans espaces;
+	//check if ants. (or negative ants).
+	//check if ant nbr is int size.
 	char *line;
+
+	line = NULL;
+	if (!ft_get_next_line(0, &line) || *line == '-' ||
+		!ft_is_int_size(line) || !ft_is_digit_str(line))
+	{
+		ft_printf("Error ant value.\n");
+		exit(EXIT_FAILURE);
+	}
+	lem->nb_ants = ft_atoi(line);
+}
+	
 	while (ft_get_next_line(0, &line))
 	{
-		if (ft_is_valid(line))
-			ft_add_lstmap(lem, line);
+		if (ft_is_ant(line) && lem->nb_ant == -1)
+			lem->nb_ants = ft_atoi(line);
+		else if (ft_is_room(line))
+			ft_add_room(lem, line);
+		else if (ft_is_link(line))
+			ft_add_link(lem, line);
+		else if (*line == '#')
+			ft_get_cmd(lem, line);
 		else
 			break ;
 	}
-}
 
 int 	ft_is_valid(char *str)
 {
-	//nombre fourmis == une chaine de digit;
-
-	//chambre == "name", ' ', "digit", ' ', "digit".
-
-	//connections == "name", '-', "name".
-
 	//commandes == "##cmd"
+	//check if valid command. (if ##).
+
+	
+	//		ROOMS
+	//chambre == "name", ' ', "digit", ' ', "digit".
+	//check duplicated name.
+	//check nb de coordonees.
+	//check if letter in coord.
+	//check if start and end room.
+	
+	//		LINKS
+	//links == "name", '-', "name".
+	//check if room exist when linking parsing.
+	//check if one tiret in links.
 	int i;
 
 	i = 0;
-	if (*str == '#' && *(str + 1) != '#')
-		return (0);
 	while (str[i])
 	{
 		if (str[i])
